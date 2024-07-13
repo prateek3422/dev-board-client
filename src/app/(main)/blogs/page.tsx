62affe19ec907a1fe394eddeb4195abcfa156805
prateek3422 <123472716+prateek3422@users.noreply.github.com>
2024-07-13 13:26:05 +0530

    fix bg image

--- src/app/(main)/blogs/page.tsx
+++ src/app/(main)/blogs/page.tsx
@@ -35,7 +35,7 @@ export default  function Blogs() {
   return (
     <div className="mt-[88px] ">
       <header className="flex justify-center items-center ">
-      <form className="form relative">
+      {/* <form className="form relative"> */}
       <input
         className="input rounded-full px-8 py-3 border-2 w-[40vw] border-transparent focus:outline-none focus:border-blue-500 placeholder-gray-400 transition-all duration-300 bg-[#4e4e4f] shadow-md"
         placeholder="Search..."
@@ -47,7 +47,7 @@ export default  function Blogs() {
       <Button className="absolute right-3 -translate-y-1/2 top-1/2 p-1 w-[40px] rounded-full bg-blue-600 hover:bg-blue-700 text-white">
         <FaSearch />
       </Button>
-    </form>
+    {/* </form> */}
       </header>
 
       <section className=" grid sm:grid-cols-2 md:grid-cols-3 mt-16 max-w-screen-4xl mx-auto gap-4 px-4">
